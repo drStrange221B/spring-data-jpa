5d9c25e1585b2933618c723830842e7f489af69f
rojayamaharjan <work.work.mh3@gmail.com>
2022-01-19 15:26:23 -0600

    note

--- src/main/java/com/emrys/conference/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.emrys.conference.repository;
-
-import com.emrys.conference.model.User;
-
-public interface UserRepository {
-    User save(User user);
-}
--- src/main/java/com/emrys/conference/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.emrys.conference.service;
-
-import com.emrys.conference.model.User;
-
-public interface UserService {
-    User save(User user);
-}
